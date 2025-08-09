3c43126a0fa04e8f5dad510021588d6da1d0ea8e
vimalsolanki2006 <vimalsolanki8012@gmail.com>
2025-08-09 16:48:08 +0000

    typeconversion

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert(Hello);    // we are using, not browser so it gives error
+
+let name = "hitesh";
+let age = 18;
+let isLoogedIn = false;
+
+// number => range is 2^53
+// bigint
+// string => single and double quote both are allowed
+// boolean => true/false
+// null => stand alone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "JAVASCRIPT"); //string
+console.log(typeof age); // number
+console.log(typeof null); // null is a object
+console.log(typeof undefined); // undefined
