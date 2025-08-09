504e7e3449cbfb1319b0eec66cfa7e11f08e5bd0
Vimal Solanki <vimalsolanki8012@gmail.com>
2025-08-09 06:13:26 +0000

    varible

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "test12@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jamnagar"
+
+//accountId = 2  // not allowed
+
+console.log(accountId);
